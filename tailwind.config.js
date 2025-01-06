5838e596bb6dc862145340c4634926ef4eee447d
moonye@live.fr <moonye@live.fr>
2025-01-06 13:42:13 +0100

    ADD : page index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./FRONT-END/HTML/*.html"],
+  content: ["./FRONT-END/HTML/*.html", "./FRONT-END/PHP/**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#121213",
+        white: "#EAEAEA",
+        violet: "#1C161E",
+        yellow: "#EFE3C8",
+        brown: "#704341",
+        "yellow-transparent": "rgba(239, 227, 200, 0.7)", // Jaune transparent à 70%
+      },
+      fontFamily: {
+        rosarivo: ["Rosarivo", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+        rosario: ["Rosario", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
